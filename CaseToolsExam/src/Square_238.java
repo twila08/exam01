0b3d1cff7b8bdec6fa4a6e3ef9d3230dd4390fc2
exam <exam@Bcalab4-m13>
2023-05-04 12:21:25 +0530

    Commit

---
+++ CaseToolsExam/src/Square_238.java
@@ -0,0 +1,15 @@
+
+public class Square_238 {
+	int length;
+	
+	Square_238(int lenght)
+	{
+		this.length = lenght;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
